dc6f21f90dbb16ff0c7e07eeffefa129e80a1352
jornJp0201 <uda.job.hunting@gmail.com>
2026-07-15 09:24:05 +0900

    前回と同様にコードを見やすくしました（昨日の追加は行っていません）

--- src/main/java/com/example/test/TaxtRepository.java
+++ src/main/java/com/example/test/TaxtRepository.java
@@ -8,9 +8,9 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
-//@Repository
+
 public interface TaxtRepository extends JpaRepository<TaxtEntity, String> {
-    // nativeQuery=true を外し、Entityクラス名（TaxtEntity）とフィールド名（taxt）で指定する
+    
 @Query("SELECT t.taxt FROM TaxtEntity t")
 List<String> All_getTitle();
 }
